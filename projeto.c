342f9aec3f672be61e70e4b23a5607f2da7cd5a7
kikofmas <kikofmas@gmail.com>
2019-03-04 19:31:01 +0000

    Teste 1

--- projeto.c
+++ projeto.c
@@ -1,4 +1,14 @@
+/*Projeto de programação de
+*Bruno Cebola - 93030
+*Frederico Maria Almeida Santos - 93065*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
+int main() {
+  
+  return 0;
+}
