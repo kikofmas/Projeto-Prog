cef2facdb31a79fbb0b86058abdf5e2b379786ae
kikofmas <kikofmas@gmail.com>
2019-03-06 11:36:11 +0000

    TESTE 2

--- projeto.c
+++ projeto.c
@@ -9,6 +9,19 @@
 #include <string.h>
 
 int main() {
-  
+  int jog;
+  printf("Vamos jogar um jogo de mastermind!\n");
+
+  while (jog<1 || jog>4) {
+    printf("Insira o numero de jogadores:  ");
+    if (scanf("%d", &jog)==0) {
+      fflush(stdin);
+      scanf("%s", &jog);
+      printf("Erro: numero de jogadores invalido\n");
+      continue;
+    }
+  }
+  printf("%d", jog);
+
   return 0;
 }
