a2b1f1cbfa61527db54216c2274d7edbdc86cbf4
kikofmas <kikofmas@gmail.com>
2019-03-06 12:13:07 +0000

    Quarta 6/03

--- projeto.c
+++ projeto.c
@@ -2,13 +2,27 @@
 *Bruno Cebola - 93030
 *Frederico Maria Almeida Santos - 93065*/
 
-
+#include <locale.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
+
+
+typedef struct{
+  char name[21];
+  int numjog;
+  int t;
+}jogadores;
+
 
 int main() {
+  setlocale(LC_ALL,"");
+
+  jogadores jogador[4];
+
+  char err[1];
   int jog;
   printf("Vamos jogar um jogo de mastermind!\n");
 
@@ -16,12 +30,17 @@ int main() {
     printf("Insira o numero de jogadores:  ");
     if (scanf("%d", &jog)==0) {
       fflush(stdin);
-      scanf("%s", &jog);
+      scanf("%s", err);
       printf("Erro: numero de jogadores invalido\n");
       continue;
     }
   }
-  printf("%d", jog);
+  for (int i = 0; i < jog; i++) {
+    fgets(jogador[i].name, 20, stdin);
+  }
+  for (int i = 0; i < jog; i++) {
+
+  }
 
   return 0;
 }
