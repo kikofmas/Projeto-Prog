4ca396ef7cc9c9141a631b0d587dcdaaf128e4cb
kikofmas <kikofmas@gmail.com>
2019-03-13 18:35:59 +0000

    sgd

--- testground.c
+++ testground.c
@@ -1,14 +1,1 @@
-char key[9];
-char coresdisp[13]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'};
-for (int i = 0; i < keySize; i++) {
-  int aux;
-  do{
-    aux = rand() % colors;
-  } while(coresdisp[aux]=='0');
-  key[i]=coresdisp[aux];
-  if(repet=='n' || repet=='N'){
-    coresdisp[aux]='0';
-  }
-}
 
-printf("%s\n", key);
