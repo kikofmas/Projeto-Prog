1e69109ad83d16972c86516eeef561f7954c72f5
kikofmas <kikofmas@gmail.com>
2019-03-16 15:46:11 +0000

    jboh

--- testground.c
+++ testground.c
@@ -1,5 +1,4 @@
-// C program to demonstrate
-// example of time() function.
+//testground: code and test functions before inserting them in main project
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,7 +6,7 @@
 int main (){
   int jog=0, tempo=0, games=0; //numero de jogadores, tempo de jogo, numero de jogos
   int stats[jog][games][3];//0=temp, 1=jogadas, 2=conseguiu
-
+  int mediaTempos[jog];
   stats[i][a][0]=tempo_jogo;
   stats[i][a][1]=b;
 
@@ -15,11 +14,26 @@ int main (){
   stats[i][a][2]=1;
   stats[i][a][2]=0;
 
+
   for (int i = 0; i < jog; i++) {
-    int z, x;
+    mediaTempos[i]=
+  }
+
+  for (int i = 0; i < jog; i++) {
+    int z=0, x=0, y=0;
+    y=x;
     x=0;
+
     for (int u = 0; u < games; u++) {
-      /* code */
+      x+=stats[i][u][2];
+    }
+    if (x>y) {
+      z=i;
+      y=x;
+    } else if (x==y) {
+      if (/* condition */) {
+        /* code */
+      }
     }
   }
 
