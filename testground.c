4958c44007c7f13b2d011d6641d0f308730eaa0f
kikofmas <kikofmas@gmail.com>
2019-03-16 13:46:48 +0000

    srgsd

--- testground.c
+++ testground.c
@@ -1,18 +1,28 @@
 // C program to demonstrate
 // example of time() function.
 #include <stdio.h>
-#include <time.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-int main ()
-{
-    time_t inicial,medio,final;
 
-    inicial = time(NULL);
-    sleep(3);
-    medio = time(NULL);
-    sleep(3);
-    medio = time(NULL);
-    printf("%d\n", medio-inicial);
+int main (){
+  int jog=0, tempo=0, games=0; //numero de jogadores, tempo de jogo, numero de jogos
+  int stats[jog][games][3];//0=temp, 1=jogadas, 2=conseguiu
+
+  stats[i][a][0]=tempo_jogo;
+  stats[i][a][1]=b;
+
+
+  stats[i][a][2]=1;
+  stats[i][a][2]=0;
+
+  for (int i = 0; i < jog; i++) {
+    int z, x;
+    x=0;
+    for (int u = 0; u < games; u++) {
+      /* code */
+    }
+  }
+
+
     return(0);
 }
