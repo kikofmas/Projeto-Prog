7da2d1ce4d413c8b61d4d34f9ed258df3061d039
MEEC PROG 1718 <student@localhost.localdomain>
2019-04-28 01:21:12 +0100

    kkk

--- Projeto_Final/Testgrounds/testground_bruno.c
+++ Projeto_Final/Testgrounds/testground_bruno.c
@@ -1,4 +1,17 @@
 /****************************
-/*       testground BRUNO   *
+       testground BRUNO
 ****************************/
-aa
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>   //funcoes de strings
+#include <time.h>     //usada para se poder calcular o tempo
+#include <ctype.h>    //toupper and tolower functions
+#include <unistd.h>   //permite usar a funcao sleep
+#include <termios.h>  //permite usar a funcao tcflush
+#include "oraculo.h"
+
+int main() {
+  activate_oracle(4, 6, 0);
+
+  return 0;
+}
