39167155132ca1248caae26b6ddd3f0c4515efa5
kikofmas <kikofmas@gmail.com>
2019-05-06 22:23:01 +0100

    Merge branch 'master' of https://github.com/kikofmas/Projeto-Prog

--- Projeto_Final/Testgrounds/kiko/testground2_kiko.c
+++ Projeto_Final/Testgrounds/kiko/testground2_kiko.c
@@ -10,3 +10,33 @@
 #include <ctype.h>    //toupper and tolower functions
 #include <unistd.h>   //permite usar a funcao sleep
 #include <termios.h>  //permite usar a funcao tcflush
+
+
+typedef struct guess_t {
+  int guess_ID;
+  char *guess;
+  char result[5];
+  guess_t *next;
+}guess_list;
+
+
+typedef struct game_registry {
+  int game_ID;
+  char player_ID[5];
+  char *player_name;
+  int colors;
+  int key_size;
+  char repet;
+  char *key;
+  int game_time;
+  struct game_registry *next;
+  try *first;
+}game_reg;
+
+
+
+
+int main(int argc, char const *argv[]) {
+  game_reg *registo_jogo;
+  return 0;
+}
