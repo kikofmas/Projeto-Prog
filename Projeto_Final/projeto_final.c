6a137174a4872fe0eb171562a3cc521413a9a291
MEEC PROG 1718 <bruno.m.cebola@gmail.com>
2019-05-06 16:03:22 +0100

    kkk

--- Projeto_Final/projeto_final.c
+++ Projeto_Final/projeto_final.c
@@ -16,7 +16,7 @@
 #include <time.h>     //usada para se poder calcular o tempo
 #include <ctype.h>    //toupper and tolower functions
 #include <unistd.h>   //permite usar a funcao sleep
-//#include <termios.h>  //permite usar a funcao tcflush
+#include <termios.h>  //permite usar a funcao tcflush
 
 
 //DECLARACAO DE ESTRUTURAS
