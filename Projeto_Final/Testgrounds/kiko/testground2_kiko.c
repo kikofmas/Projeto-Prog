aef3f04d37b343411ead85848df9d47ed171edd6
kikofmas <kikofmas@gmail.com>
2019-05-06 21:34:32 +0100

    aaa

---
+++ Projeto_Final/Testgrounds/kiko/testground2_kiko.c
@@ -0,0 +1,12 @@
+/****************************
+*       testground KIKO    *
+****************************/
+
+#include <stdlib.h>
+#include <errno.h>    //library de error handling
+#include <stdio.h>
+#include <string.h>   //funcoes de strings
+#include <time.h>     //usada para se poder calcular o tempo
+#include <ctype.h>    //toupper and tolower functions
+#include <unistd.h>   //permite usar a funcao sleep
+#include <termios.h>  //permite usar a funcao tcflush
