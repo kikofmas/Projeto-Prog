1c201031de41fae75fe047d2328e05ed59b99975
kikofmas <kikofmas@gmail.com>
2019-05-07 11:55:20 +0100

    aa

--- Projeto_Final/testgrounds_e_historico/kiko/testground_kiko.c
+++ Projeto_Final/testgrounds_e_historico/kiko/testground_kiko.c
@@ -20,6 +20,13 @@ typedef struct guess_t {
 }guess_list;
 
 
+typedef struct {
+  int ID;
+  char player_ID[5];
+}hist_data;
+
+
+
 typedef struct game_registry {
   int game_ID;
   char player_ID[5];
@@ -38,5 +45,26 @@ typedef struct game_registry {
 
 int main(int argc, char const *argv[]) {
   game_reg *registo_jogo;
+  hist_data *last_game;
+  int pid=0;
+
+
+  (**registo_jogo) //argumento funcoes
+  (&registo_jogo) //argumento main
+
+  game_reg *current_player;
+  current_player=*registo_jogo;
+
+
+  current->game_ID=((last_game->ID)+1);
+  last_game->player_ID[0]="0";
+  pid=atoi(last_game->player_ID);
+  pid++;
+  current->player_ID=pid; //FALTA POR PA STRING
+  current->player_ID[0]="J";
+
+
+
+
   return 0;
 }
