339b3605bc93c7c86a5d55958d2d48b5e1b0dbbb
MEEC PROG 1718 <bruno.m.cebola@gmail.com>
2019-05-20 19:02:15 +0100

    aa

--- Projeto_Final/testgrounds_e_historico/bruno/bruno.c
+++ Projeto_Final/testgrounds_e_historico/bruno/bruno.c
@@ -26,3 +26,52 @@ int main(int argc, char const *argv[]) {
 
   return 0;
 }
+
+
+
+
+
+
+void ordena(game_reg **reg){
+  game_reg *atual, *check;
+  atual = *reg;
+  check = *reg;
+  while(atual!=NULL){
+    while(check!=NULL){
+      if(atual->key_size > check->key_size){
+        swap(&atual,&check);
+      }
+      else if(atual->key_size == check->key_size &&
+              atual->colors > check->colors){
+        swap(&atual,&check);
+      }
+      else if(atual->key_size == check->key_size &&
+              atual->colors == check->colors &&
+              atual->repet=='S' && check->repet=='N'){
+        swap(&atual,&check);
+      }
+      else if(atual->key_size == check->key_size &&
+              atual->colors == check->colors &&
+              atual->repet == check->repet &&
+              atual->tentativas > check->tentativas){
+        swap(&atual,&check);
+      }
+      else if(atual->key_size == check->key_size &&
+              atual->colors == check->colors &&
+              atual->repet == check->repet &&
+              atual->tentativas == check->tentativas &&
+              atual->game_time == check->game_time){
+        swap(&atual,&check);
+      }
+      check = check->next;
+    }
+    atual=atual->next;
+  }
+}
+
+
+void swap(game_reg **ptr1, game_reg **ptr2){
+  (*ptr2)->prev = (*ptr1)->prev;
+  (*ptr1)->next = (*ptr2)->next;
+
+}
