62dc43b4bac8ba7c1d16bbfb73a3593ba9d64347
MEEC PROG 1718 <bruno.m.cebola@gmail.com>
2019-05-22 17:25:03 +0100

    kkk

--- Projeto_Final/testgrounds_e_historico/bruno/source/intermedio.h
+++ Projeto_Final/testgrounds_e_historico/bruno/source/intermedio.h
@@ -14,7 +14,7 @@ char createKey(char chave[8], char repeticao_cores, int tamanho_chave, int num_c
 void comparaChave(int tamanho_chave, char jogada[8], char copia_chave[8], int *lugar_certo, int *lugar_errado); //comparacao com a chave de jogo
 int userAttempt(dados **ptr_dados, char ultima_cor, char *jogada, int tamanho_chave, time_t tempo_inicial,
                 time_t *tempo_jogo, time_t *tempo_restante, int duracao_jogo, int num_cores, int jogador, int jogo); //tentativa do jogador
-dados **jogo(defs def, char **nome_jogadores);  //funcao que permite jogar
+dados **jogo(defs def, char **nome_jogadores, game_reg **registo_jogo, hist_data *last_game);  //funcao que permite jogar
 void criaDados(int num_jogadores, int num_jogos, dados **ptr_dados, float **mediaTempos, int **numVitorias); //criacao da media de tempo de jogo de cada jogador
 void vencedor(float *mediaTempos, char **nome, int num_jogadores, int *numVitorias); //definicao do vencedor do jogo
 void resultados(int num_jogadores, int num_jogos, dados **ptr_dados, int dado_principal, char frase[], char **nome); //apresenta as estatisticas
--- Projeto_Final/testgrounds_e_historico/bruno/source/main.c
+++ Projeto_Final/testgrounds_e_historico/bruno/source/main.c
@@ -24,9 +24,7 @@
 
 
 //DECLARACAO DE FUNCOES
-void save_game_ini(game_reg **registo_jogo, int hist_file, int ord, hist_data last_game, char **nome_jogadores, defs defs_jogo, int jogador);
-void save_key(int k, game_reg *registo_jogo, char jogada[]);
-void save_guess_ini(game_reg *top, int lugar_certo, int lugar_errado, int tentativa, defs defs_jogo, char jogada[]);
+
 
 
 
@@ -50,6 +48,8 @@ int main(int argc, char const *argv[]) {
   dados **ptr_dados=NULL; //[][][0]=tempo, [][][1]=tentativas, [][][2]=vitoria
   int *numVitorias=NULL;
   float *mediaTempos=NULL;
+
+  game_reg = *registo_jogo;
 
 //inicializacao da funcao srand:
   time_t t;
@@ -92,7 +92,7 @@ int main(int argc, char const *argv[]) {
 
     //JOGO
       //activate_oracle(defs_jogo.tamanho_chave, defs_jogo.num_cores, rep);
-      ptr_dados=jogo(defs_jogo, nome_jogadores);
+      ptr_dados=jogo(defs_jogo, nome_jogadores, &registo_jogo, &last_game);
     //ESTATISTICAS: calculo dos resultados e apresentacao das estatisticas
 
       criaDados(defs_jogo.num_jogadores, defs_jogo.num_jogos, ptr_dados, &mediaTempos, &numVitorias);
@@ -126,108 +126,7 @@ int main(int argc, char const *argv[]) {
 
 
 
-void save_game_ini(game_reg **registo_jogo, int hist_file, int ord, hist_data *last_game, char **nome_jogadores, defs defs_jogo, int jogador){
-  int pid=0;
-  static int k=0; //se houver ficheiro hmmmmm
-  game_reg *current_game=*registo_jogo; //chama-se current mas isso so e vdd para a primeira vez, a partir dai e o anterior
-  //inicio de jogo
-  /*isto so funciona se leres o ficheiro -h antes e meteres o pointer a apontar para a lista
-  caso se implemente de maneira diferente tenho de mudar isto*/
-  if (k==0 && !(hist_file!=0 && ord!=0)) {  //verifica se primeiro elemento da lista esta preenchido
-    registo_jogo=calloc(1, sizeof(game_reg));
-    current_game=*registo_jogo; //se e a primeira vez que se passa aqui estrutura esta nao esta alocada, dai voltar a fazer isto
 
-    current_game->game_ID=((last_game.ID)+1);
-    (last_game.ID)++;
-    current_game->player_name=calloc(strlen(*(nome_jogadores+jogador))+1, sizeof(char));
-    strcpy(current_game->player_name, *(nome_jogadores+jogador));
-    sprintf(current_game->player_ID, "J%03d", (last_game.player_ID)+1);
-    (last_game.player_ID)++;
-    current_game->colors=defs_jogo.num_cores;
-    current_game->key_size=defs_jogo.tamanho_chave;
-    current_game->repet=defs_jogo.repeticao_cores;
-    current_game->game_time=defs_jogo.duracao_jogo;
-    current_game->prev=last_game.last;
-    last_game.last=current_game;
-    current_game->next=NULL;
-    k=1;
-  } else {
-    while (current_game->next != NULL){
-      current_game = current_game->next;
-    }
-    current_game->next=calloc(1, sizeof(game_reg));
-    current_game->next->game_ID=((last_game.ID)+1);
-    (last_game.ID)++;
-    current_game->next->player_name=calloc(strlen(*(nome_jogadores+jogador))+1, sizeof(char));
-    strcpy(current_game->next->player_name, *(nome_jogadores+jogador));
-    if (strcmp(current_game->player_name, current_game->next->player_name) == 0) {
-      strcpy(current_game->next->player_ID, current_game->player_ID);
-    } else {
-      sprintf(current_game->next->player_ID, "J%03d", (last_game.player_ID)+1);
-      (last_game.player_ID)++;
-    }
-    current_game->next->colors=defs_jogo.num_cores;
-    current_game->next->key_size=defs_jogo.tamanho_chave;
-    current_game->next->repet=defs_jogo.repeticao_cores;
-    current_game->next->game_time=defs_jogo.duracao_jogo;
-    current_game->next->prev=last_game.last;
-    last_game.last=current_game->next;
-    current_game->next->next=NULL;
-  }
-}
-
-void save_key(int k, game_reg *registo_jogo, char jogada[]){
-  game_reg *current_game = registo_jogo;
-  char travessao[] = "-";
-  while (current_game->next != NULL){
-    current_game = current_game->next;
-  }
-  /*onde se grava a vitoria*/
-  if(k==1) {
-    current_game->key=calloc((strlen(jogada))+1, sizeof(char));
-    strcpy(current_game->key, jogada);
-  }
-  /*onde se mete a derrota*/
-  if(k==0) {
-    current_game->key=calloc((strlen(travessao))+1, sizeof(char));
-    strcpy(current_game->key, travessao);
-  }
-}
-
-void save_guess_ini(game_reg *top, int lugar_certo, int lugar_errado, int tentativa, defs defs_jogo, char jogada[]) {
-  game_reg *current_game=top;
-  tentativas *current_guess;
-
-  while (current_game->next != NULL){
-    current_game = current_game->next;
-  }
-  if (tentativa==0) {  //verifica se primeiro elemento da lista esta preenchido
-    current_game->tentativas=tentativa+1;
-    current_game->first=calloc(1, sizeof(tentativas));
-    current_guess=current_game->first; //se e a primeira vez que se passa aqui estrutura esta nao esta alocada, dai voltar a fazer isto
-
-    current_guess->tent_ID=tentativa+1;
-    current_guess->tentativa=calloc((strlen(jogada))+1, sizeof(char));
-    strcpy(current_guess->tentativa, jogada);
-    sprintf(current_guess->resultado, "P%1dB%1d", lugar_certo, lugar_errado);
-    current_guess->prev=NULL;
-    current_guess->next=NULL;
-    k=1;
-  } else {
-    current_guess=current_game->first;
-    while (current_guess->next != NULL){
-      current_guess = current_guess->next;
-    }
-    current_game->tentativas=tentativa+1;
-    current_guess->next=calloc(1, sizeof(tentativas));
-    current_guess->next->tent_ID=tentativa+1;
-    current_guess->next->tentativa=calloc((strlen(jogada))+1, sizeof(char));
-    strcpy(current_guess->next->tentativa, jogada);
-    sprintf(current_guess->next->resultado, "P%1dB%1d", lugar_certo, lugar_errado);
-    current_guess->next->prev=current_guess;
-    current_guess->next->next=NULL;
-  }
-}
 
 
 
@@ -255,3 +154,4 @@ void free_guess_list(tentativas *current){
   }
   free(current);
 }
+
